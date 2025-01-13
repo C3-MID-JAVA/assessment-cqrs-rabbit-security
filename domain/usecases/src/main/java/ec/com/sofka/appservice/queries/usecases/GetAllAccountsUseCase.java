176eb1f8f692c17cb41666264e19ad595df480f6
ANDERSON CRISTOBAL ZAMBRANO BALSECA <zambranoanderson84@yahoo.es>
2025-01-13 16:53:28 -0500

    vale getAll

--- domain/usecases/src/main/java/ec/com/sofka/appservice/queries/usecases/GetAllAccountsUseCase.java
+++ domain/usecases/src/main/java/ec/com/sofka/appservice/queries/usecases/GetAllAccountsUseCase.java
@@ -1,20 +1,13 @@
 package ec.com.sofka.appservice.queries.usecases;
 
-import ec.com.sofka.aggregate.Customer;
-import ec.com.sofka.aggregate.events.AccountCreated;
-import ec.com.sofka.aggregate.events.AccountUpdated;
 import ec.com.sofka.appservice.queries.responses.AccountResponse;
 import ec.com.sofka.appservice.gateway.IBusEvent;
 import ec.com.sofka.appservice.gateway.IAccountRepository;
 import ec.com.sofka.appservice.gateway.IEventStore;
-import ec.com.sofka.generics.domain.DomainEvent;
 import ec.com.sofka.generics.interfaces.IUseCaseGetEmpty;
 import ec.com.sofka.generics.utils.QueryResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class GetAllAccountsUseCase implements IUseCaseGetEmpty<AccountResponse> {
 
@@ -33,36 +26,19 @@ public class GetAllAccountsUseCase implements IUseCaseGetEmpty<AccountResponse> 
 
     @Override
     public Mono<QueryResponse<AccountResponse>> get() {
-        return eventRepository.findAllAggregates()
-                .collectList()
-                .flatMap(events -> {
-                    // Filtrar eventos por tipo de cuenta
-                    Map<String, DomainEvent> mapLatestEvents = events.stream()
-                            .filter(event -> event instanceof AccountCreated || event instanceof AccountUpdated)
-                            .collect(Collectors.toMap(
-                                    DomainEvent::getAggregateRootId,
-                                    event -> event,
-                                    (existing, replacement) -> existing.getVersion() >= replacement.getVersion() ? existing : replacement
-                            ));
-
-                    Flux<DomainEvent> latestEventsFlux = Flux.fromIterable(mapLatestEvents.values());
-
-                    return latestEventsFlux
-                            .flatMap(event -> {
-                                return Customer.from(event.getAggregateRootId(), Flux.fromIterable(mapLatestEvents.values()));
-                            })
-                            .map(customer -> new AccountResponse(
-                                    customer.getId().getValue(),
-                                    customer.getAccount().getId().getValue(),
-                                    customer.getAccount().getAccountNumber().getValue(),
-                                    customer.getAccount().getOwner().getValue(),
-                                    customer.getAccount().getBalance().getValue(),
-                                    customer.getAccount().getStatus().getValue()
-                            ))
-                            .collectList()
-                            .flatMap(responses -> Mono.just(QueryResponse.ofMultiple(responses)));
-                });
+        return repository.findAll()
+                .map(accountDTO -> new AccountResponse(
+                        accountDTO.getAccountId(),
+                        accountDTO.getAccountId(),
+                        accountDTO.getAccountNumber(),
+                        accountDTO.getName(),
+                        accountDTO.getBalance(),
+                        accountDTO.getStatus()
+                ))
+                .collectList() // Convierte el Flux en una lista
+                .map(QueryResponse::ofMultiple); // Envuelve la lista en un QueryResponse
     }
+
 
 
 }
